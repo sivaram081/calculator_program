3f4f0d1787430a2fae4ae7310c0868293ce853a3
shivs <sivakodali27@gmail.com>
2025-10-23 19:46:33 -0400

    functions updated in code

--- calculator.py
+++ calculator.py
@@ -1,17 +1,14 @@
-# simple Calculator
+# calculator.py
 
-# This function adds two numbers
 def add(x, y):
     return x + y
-# This function subtracts two numbers
+
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
     return x / y
 
