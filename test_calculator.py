286127318de6dcb262df6f9e82e5ce3dbc412e1a
shivs <sivakodali27@gmail.com>
2025-10-23 20:00:52 -0400

    code updated

--- test_calculator.py
+++ test_calculator.py
@@ -2,20 +2,31 @@
 import unittest
 from calculator import add, subtract, multiply, divide
 
+
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(10, 5), 50)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
 
     def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(-10, 2), -5)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
 
